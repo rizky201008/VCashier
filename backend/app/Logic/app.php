0b5d0bcc4aff58ba91a6d0080f5b11b4d9225bfd
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-02-16 20:11:12 +0700

    Add App class with createSlug method

---
+++ backend/app/Logic/app.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Logic;
+
+use Illuminate\Support\Str;
+
+class App {
+
+    public function createSlug($name)
+    {
+        return Str::slug(round(microtime(true) * 1000) . '-' . $name, '-');
+    }
+
+}

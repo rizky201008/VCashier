513bf353f3810906b6a5b910d51a7ad54422db2e
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-02-16 20:11:31 +0700

    Update Category model and migration

--- backend/database/migrations/2024_02_15_161719_create_categories_table.php
+++ backend/database/migrations/2024_02_15_161719_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('slug')->unique();
+            $table->text('slug')->unique();
             $table->timestamps();
         });
     }

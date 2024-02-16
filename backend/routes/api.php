4cde19ad0519564ce2fb3975744651ab429190df
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-02-16 20:10:27 +0700

    modify api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -21,15 +21,18 @@ use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('categories', [CategoryController::class, 'getCategories']);
+    Route::get('categories/{slug}', [CategoryController::class, 'getCategory']);
+    Route::post('categories', [CategoryController::class, 'createCategory']);
+    Route::post('categories', [CategoryController::class, 'createCategory']);
+    Route::put('categories', [CategoryController::class, 'updateCategory']);
+    Route::delete('categories/{id}', [CategoryController::class, 'deleteCategory']);
 });
 
-Route::prefix('/auth')->group(function () {
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-    Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
-    Route::post('/reset-password', [AuthController::class, 'resetPassword']);
-    Route::post("/change-password", [AuthController::class, 'changePassword'])->middleware('auth:sanctum');
+Route::prefix('auth')->group(function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    Route::post("change-password", [AuthController::class, 'changePassword'])->middleware('auth:sanctum');
 });

2963b1d01518406a878e3531ba68fd2f80b98eca
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-03-02 23:16:30 +0700

    refactor: move file

--- mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/MainActivity.kt
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.vixiloc.vcashiermobile
+package com.vixiloc.vcashiermobile.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.vixiloc.vcashiermobile.ui.theme.VcashierMobileTheme
+import com.vixiloc.vcashiermobile.presentation.ui.theme.VcashierMobileTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

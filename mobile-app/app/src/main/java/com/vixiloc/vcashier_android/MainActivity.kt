e87c4bac08f71e638b7ffee0cd5bccdcdfeb3dfc
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-03-02 20:16:12 +0700

    add mobile project

---
+++ mobile-app/app/src/main/java/com/vixiloc/vcashier_android/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.vixiloc.vcashier_android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.vixiloc.vcashier_android.ui.theme.VcashierandroidTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            VcashierandroidTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    VcashierandroidTheme {
+        Greeting("Android")
+    }
+}

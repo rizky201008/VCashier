8df653fba3bf4ed41b09d3039c860964ad756aa4
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-03-07 09:25:25 +0700

    refactor: modify methods and remove unused imports

--- backend/app/Http/Controllers/ProductController.php
+++ backend/app/Http/Controllers/ProductController.php
@@ -2,25 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Product;
+use App\Data\ProductRepository;
+use Exception;
 use Illuminate\Http\Request;
-use App\Logic\App as AppLogic;
-use App\Models\ProductVariation;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
-    protected $appLogic;
-    protected $product;
-    protected $variation;
+
+    protected ProductRepository $productRepository;
 
     public function __construct()
     {
-        $this->appLogic = new AppLogic();
-        $this->product = new Product();
-        $this->variation = new ProductVariation();
+        $this->productRepository = new ProductRepository();
     }
+
     public function createProduct(Request $request)
     {
         $request->validate([
@@ -30,37 +26,7 @@ class ProductController extends Controller
             'variations' => 'required|array|min:1',
         ]);
 
-        DB::beginTransaction();
-
-        try {
-            $product = Product::create([
-                'name' => $request->name,
-                'description' => $request->description,
-                'slug' => $this->appLogic->createSlug($request->name),
-                'category_id' => $request->category_id
-            ]);
-
-            $productVariations = $request->variations;
-
-            foreach ($productVariations as $variation) {
-                $this->variation->create([
-                    'name' => $variation['name'],
-                    'stock' => $variation['stock'],
-                    'price' => $variation['price'],
-                    'price_grocery' => $variation['price_grocery'],
-                    'product_id' => $product->id
-                ]);
-            }
-
-            DB::commit();
-            return response()->json([
-                'message' => 'Product created successfully'
-            ], 201);
-        } catch (\Throwable $th) {
-            DB::rollBack();
-
-            throw $th;
-        }
+        return response()->json($this->productRepository->createProduct($request));
     }
 
     public function updateProduct(Request $request, $id)
@@ -71,47 +37,10 @@ class ProductController extends Controller
         ]);
 
         if ($validator->fails()) {
-            throw $validator->messages()->first();
+            throw new Exception($validator->messages()->first());
         }
 
-        $product = Product::find($id);
-
-        if ($request->name !== $product->name) {
-            $product->slug = $this->appLogic->createSlug($request->name);
-        }
-
-        DB::beginTransaction();
-
-        try {
-
-            $product->update($request->all());
-
-            $variations = $request->variations;
-
-            if ($variations !== null) {
-                $product->variations()->delete();
-                foreach ($variations as $variation) {
-                    $this->variation->create(
-                        [
-                            'name' => $variation['name'],
-                            'stock' => $variation['stock'],
-                            'price' => $variation['price'],
-                            'price_grocery' => $variation['price_grocery'],
-                            'product_id' => $product->id
-                        ]
-                    );
-                }
-            }
-
-            DB::commit();
-            return response()->json([
-                'message' => 'Product updated successfully'
-            ], 200);
-        } catch (\Throwable $th) {
-            DB::rollBack();
-
-            throw $th;
-        }
+        return response()->json($this->productRepository->updateProduct($request, $id));
     }
 
     public function deleteProduct($id)
@@ -121,47 +50,19 @@ class ProductController extends Controller
         ]);
 
         if ($validator->fails()) {
-            throw $validator->messages()->first();
+            throw new Exception($validator->messages()->first());
         }
 
-        DB::beginTransaction();
-
-        try {
-            $product = Product::where('id', $id)->first();
-            $product->variations()->delete();
-            $product->images()->delete();
-            $product->delete();
-
-            DB::commit();
-            return response()->json([
-                'message' => 'Product deleted successfully'
-            ], 200);
-        } catch (\Throwable $th) {
-            DB::rollBack();
-
-            throw $th;
-        }
+        return response()->json($this->productRepository->deleteProduct($id));
     }
 
-    public function getProduct($slug)
+    public function getProduct($id)
     {
-        $validator = Validator::make(['slug' => $slug], [
-            'slug' => 'required|exists:products,slug'
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json($validator->messages()->first());
-        }
-
-        $product = Product::with(['images', 'variations'])->where('slug', $slug)->first();
-
-        return response()->json($product, 200);
+        return response()->json($this->productRepository->getProduct($id));
     }
 
     public function getProducts()
     {
-        $products = Product::with(['images', 'variations'])->get();
-
-        return response()->json($products, 200);
+        return response()->json($this->productRepository->getProducts());
     }
 }

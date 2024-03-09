5b8380e18fcef55c4ca363882bf4620b94a0cfb1
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-03-09 10:22:33 +0700

    refactor: db: modify table structure

--- backend/database/migrations/2024_03_06_115525_create_customers_table.php
+++ backend/database/migrations/2024_03_06_115525_create_customers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('phone_number');
+            $table->string('phone_number')->nullable();
             $table->timestamps();
         });
     }

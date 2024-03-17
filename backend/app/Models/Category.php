7733ca317817bade9b86844b0c4ddc527c88bdb3
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-03-17 12:26:38 +0700

    refactor: backend:  remove unused field from fillable

--- backend/app/Models/Category.php
+++ backend/app/Models/Category.php
@@ -9,7 +9,7 @@ class Category extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'slug'];
+    protected $fillable = ['name'];
 
     protected $hidden = ['created_at', 'updated_at'];
 }

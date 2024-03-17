0ec1cd87a7d67e73839d017d60ff9384760541d9
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-03-17 12:15:38 +0700

    refactor: backend: modify migrations

--- backend/database/migrations/2024_02_15_161719_create_categories_table.php
+++ backend/database/migrations/2024_02_15_161719_create_categories_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('slug');
             $table->timestamps();
         });
     }
--- backend/database/migrations/2024_02_17_004935_create_products_table.php
+++ backend/database/migrations/2024_02_17_004935_create_products_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description');
-            $table->text('slug');
             $table->foreignIdFor(Category::class);
             $table->timestamps();
         });

771242d8b2ee8fd06aeb349691e3b53e3160ac81
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-03-18 16:37:27 +0700

    feat: backend: add new field

--- backend/database/migrations/2024_03_17_062318_create_transaction_items_table.php
+++ backend/database/migrations/2024_03_17_062318_create_transaction_items_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,10 @@ return new class extends Migration
     {
         Schema::create('transaction_items', function (Blueprint $table) {
             $table->id();
+            $table->string('product');
+            $table->bigInteger('price');
+            $table->bigInteger('qty');
+            $table->foreignId('transaction_id')->constrained('transactions');
             $table->timestamps();
         });
     }

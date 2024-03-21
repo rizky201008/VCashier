17e9f2671988fbbab93ef3e719e62b891da558aa
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-03-21 12:23:34 +0700

    refactor: backend: remove unused routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -40,8 +40,6 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::prefix('product-images')->group(function () {
         Route::post('create', [ProductImageController::class, 'createImages']);
         Route::post('update', [ProductImageController::class, 'updateImages']);
-        Route::delete('delete-all', [ProductImageController::class, 'deleteImages']);
-        Route::delete('delete-one', [ProductImageController::class, 'deleteImage']);
     });
     Route::prefix('customers')->group(function () {
         Route::get('/', [CustomerController::class, 'getCustomers']);

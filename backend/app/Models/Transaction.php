a5cb57ed8da51d33de21370edda6aae707197df3
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-03-22 11:22:11 +0700

    refactor: backend: modify Models, add fillable and relations

--- backend/app/Models/Transaction.php
+++ backend/app/Models/Transaction.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Transaction extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'customer_id',
+        'status',
+    ];
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(TransactionItem::class);
+    }
 }
--- backend/app/Models/TransactionItem.php
+++ backend/app/Models/TransactionItem.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class TransactionItem extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'transaction_id',
+        'product',
+        'qty',
+        'price'
+    ];
+
+    public function transaction(): BelongsTo
+    {
+        return $this->belongsTo(Transaction::class);
+    }
 }

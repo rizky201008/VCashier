14fe6b67cc775b54c061af79df085a11e03b2602
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-03-22 11:22:39 +0700

    refactor: backend: modify transactions migration

--- backend/database/migrations/2024_03_17_062257_create_transactions_table.php
+++ backend/database/migrations/2024_03_17_062257_create_transactions_table.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Models\Customer;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +14,9 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->enum('status', ['unpaid', 'completed', 'draft']);
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Customer::class);
             $table->timestamps();
         });
     }

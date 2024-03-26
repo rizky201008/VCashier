65aa07b8e4de62e29907332b7f83d2d3fc38f5de
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-03-26 13:55:51 +0700

    refactor: mobile: modify MainActivity

--- mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/MainActivity.kt
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/MainActivity.kt
@@ -4,24 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import com.vixiloc.vcashiermobile.presentation.categories.list.CategoriesListScreen
-import com.vixiloc.vcashiermobile.presentation.products.list.ProductsListScreen
+import com.vixiloc.vcashiermobile.presentation.screens.welcome.WelcomeScreen
 import com.vixiloc.vcashiermobile.presentation.ui.theme.VcashierMobileTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,32 +20,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AppScreen()
+                    WelcomeScreen()
                 }
             }
         }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun AppScreen() {
-    Scaffold(
-        topBar = {
-            TopAppBar(colors = TopAppBarDefaults.topAppBarColors(
-                containerColor = Color.Transparent
-            ), title = { /*TODO*/ }, navigationIcon = {
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(
-                        imageVector = Icons.Default.Menu,
-                        contentDescription = null,
-                        Modifier.size(50.dp)
-                    )
-                }
-            })
-        }
-    )
-    { paddingValues ->
-        ProductsListScreen(modifier = Modifier.padding(paddingValues))
     }
 }

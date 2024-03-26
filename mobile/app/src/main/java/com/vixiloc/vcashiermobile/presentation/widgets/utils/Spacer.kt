7132f838073e38ba13df9fa66ec7e237926f5d87
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-03-26 13:54:46 +0700

    feat: mobile: create Spacer

---
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/widgets/utils/Spacer.kt
@@ -0,0 +1,16 @@
+package com.vixiloc.vcashiermobile.presentation.widgets.utils
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+
+@Composable
+fun VerticalSpacer(height:Dp) {
+    Spacer(modifier = Modifier.height(height))
+}
+@Composable
+fun HorizontalSpacer(width:Dp) {
+    Spacer(modifier = Modifier.height(width))
+}

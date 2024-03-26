99d15e07e9c925bec490e1f083b6c3192ad0ce6c
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-03-26 13:55:05 +0700

    feat: mobile: create VButton

---
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/widgets/utils/VButton.kt
@@ -0,0 +1,26 @@
+package com.vixiloc.vcashiermobile.presentation.widgets.utils
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun VButton(text: String, onClick: () -> Unit, modifier: Modifier) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp)
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.padding(10.dp)
+        )
+    }
+}

705295657edda7c9a642ac125cc7fa9f198c556d
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-03-27 12:45:58 +0700

    feat: mobile: create new widget

---
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/widgets/utils/TextField.kt
@@ -0,0 +1,63 @@
+package com.vixiloc.vcashiermobile.presentation.widgets.utils
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TextField(
+    value: String,
+    onValueChanged: (String) -> Unit,
+    modifier: Modifier,
+    title: String,
+    visualTransformation: VisualTransformation = VisualTransformation.None
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+    ) {
+        Text(text = title, style = MaterialTheme.typography.bodySmall)
+        VerticalSpacer(height = 9.dp)
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+                .background(
+                    color = Color.Black.copy(alpha = 0.05f),
+                    shape = MaterialTheme.shapes.large
+                ),
+            contentAlignment = Alignment.Center
+        ) {
+            BasicTextField(
+                value = value,
+                onValueChange = onValueChanged,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                singleLine = true,
+                textStyle = MaterialTheme.typography.bodySmall,
+                visualTransformation = visualTransformation
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TextFieldPreview() {
+    TextField("", {}, Modifier, "")
+}

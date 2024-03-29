e64f538b417a256ab2fce70e823e487b1e46379a
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-03-29 22:06:36 +0700

    refactor: backend: remove unused imports

--- backend/app/Models/ProductVariation.php
+++ backend/app/Models/ProductVariation.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use App\Models\Product;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;

119ff306440058793fef2818df144f05233e5563
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-04-05 10:30:21 +0700

    feat: mobile: create new widget DropdownMenu

---
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/widgets/home/DropdownMenu.kt
@@ -0,0 +1,71 @@
+package com.vixiloc.vcashiermobile.presentation.widgets.home
+
+import android.content.ContentValues.TAG
+import android.util.Log
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DropdownMenu(
+    modifier: Modifier,
+    data: Array<String>,
+    onItemSelected: (String) -> Unit,
+    selectedText: String,
+    expanded: Boolean,
+    onExpandedChange: (Boolean) -> Unit
+) {
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = {
+            onExpandedChange(!expanded)
+        },
+        modifier = modifier
+    ) {
+        TextField(
+            value = selectedText,
+            onValueChange = {},
+            readOnly = true,
+            trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+            modifier = Modifier.menuAnchor(),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color.Transparent,
+                disabledContainerColor = Color.Transparent,
+                unfocusedContainerColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            )
+        )
+
+        ExposedDropdownMenu(
+            expanded = expanded,
+            onDismissRequest = {
+                onExpandedChange(!expanded)
+            }
+        ) {
+            data.forEach { item ->
+                DropdownMenuItem(
+                    text = { Text(text = item) },
+                    onClick = {
+                        onItemSelected(item)
+                        onExpandedChange(!expanded)
+                        Log.i(TAG, "DropdownMenu: $item")
+                    }
+                )
+            }
+        }
+    }
+}

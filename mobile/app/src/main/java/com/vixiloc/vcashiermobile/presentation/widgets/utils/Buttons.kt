3402cdc29026548ec75c6b2ce6f8a81d0127a263
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-04-12 00:32:25 +0700

    refactor: mobile: create new widgets FilledButton and OutlinedButton

--- mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/widgets/utils/Buttons.kt
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/widgets/utils/Buttons.kt
@@ -3,15 +3,21 @@ package com.vixiloc.vcashiermobile.presentation.widgets.utils
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton as OutlinedButtonCompose
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -27,8 +33,56 @@ fun IconButton(onClick: () -> Unit, icon: ImageVector) {
             .clickable { onClick() },
         contentAlignment = Alignment.Center
     ) {
-        Icon(imageVector = icon, contentDescription = null, Modifier.size(15.dp), tint = MaterialTheme.colorScheme.onPrimary)
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            Modifier.size(15.dp),
+            tint = MaterialTheme.colorScheme.onPrimary
+        )
     }
+}
+
+@Composable
+fun FilledButton(
+    onClick: () -> Unit,
+    text: String,
+    modifier: Modifier,
+    textStyle: TextStyle = MaterialTheme.typography.bodyMedium
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth(),
+        shape = MaterialTheme.shapes.large
+    ) {
+        Text(
+            text = text,
+            style = textStyle,
+            modifier = Modifier.padding(10.dp)
+        )
+    }
+}
+
+@Composable
+fun OutlinedButton(
+    onClick: () -> Unit,
+    text: String,
+    modifier: Modifier,
+    textStyle: TextStyle = MaterialTheme.typography.bodyMedium
+) {
+    OutlinedButtonCompose(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth(),
+        shape = MaterialTheme.shapes.large,
+    ) {
+        Text(
+            text = text,
+            style = textStyle,
+            modifier = Modifier.padding(10.dp)
+        )
+    }
+
 }
 
 @Preview

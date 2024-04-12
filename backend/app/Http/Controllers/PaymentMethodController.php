fdf78f2425fa005168fc1a97ba72678f7e67036e
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-04-12 13:56:19 +0700

    refactor: backend: create new controller for Payment Method

--- backend/app/Http/Controllers/PaymentMethodController.php
+++ backend/app/Http/Controllers/PaymentMethodController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Data\PaymentMethodRepository;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class PaymentMethodController extends Controller
 {
-    //
+    private $paymentMethodRepository;
+
+    public function __construct()
+    {
+        $this->paymentMethodRepository = new PaymentMethodRepository();
+    }
+
+    public function getAllPaymentMethods(Request $request): JsonResponse
+    {
+        return response()->json($this->paymentMethodRepository->getAllPaymentMethod());
+    }
 }

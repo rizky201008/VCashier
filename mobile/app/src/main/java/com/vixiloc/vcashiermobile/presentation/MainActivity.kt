d4c826b67a893497ba2c5334c3ebd9b268fe6bec
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-04-19 11:01:45 +0700

    refactor: mobile: modify MainActivity

--- mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/MainActivity.kt
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/MainActivity.kt
@@ -7,7 +7,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.vixiloc.vcashiermobile.presentation.screens.welcome.WelcomeScreen
+import com.ramcosta.composedestinations.DestinationsNavHost
+import com.vixiloc.vcashiermobile.presentation.screens.NavGraphs
 import com.vixiloc.vcashiermobile.presentation.ui.theme.VcashierMobileTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +21,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    WelcomeScreen()
+                    DestinationsNavHost(navGraph = NavGraphs.root)
                 }
             }
         }

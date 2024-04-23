7252c07b71040c7cbe23340051be1fa2132c0ff2
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-04-23 09:29:44 +0700

    refactor: mobile: modify CreateTransactionScreen

--- mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/screens/transaction/CreateTransactionScreen.kt
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/screens/transaction/CreateTransactionScreen.kt
@@ -1,30 +1,25 @@
 package com.vixiloc.vcashiermobile.presentation.screens.transaction
 
-import android.widget.Toast
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.icons.outlined.ShoppingCart
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.Dimension
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.vixiloc.vcashiermobile.presentation.screens.destinations.TransactionReviewScreenDestination
@@ -32,8 +27,8 @@ import com.vixiloc.vcashiermobile.presentation.widgets.commons.FloatingTransacti
 import com.vixiloc.vcashiermobile.presentation.widgets.commons.IconButton
 import com.vixiloc.vcashiermobile.presentation.widgets.commons.Loading
 import com.vixiloc.vcashiermobile.presentation.widgets.commons.ProductItem
+import com.vixiloc.vcashiermobile.presentation.widgets.commons.TextField
 import com.vixiloc.vcashiermobile.presentation.widgets.commons.VerticalSpacer
-import com.vixiloc.vcashiermobile.presentation.widgets.home.DropdownMenu
 import org.koin.androidx.compose.koinViewModel
 
 @Destination
@@ -57,47 +52,15 @@ fun CreateTransactionScreen(
             .background(color = Color(0xFFF6F5F5))
     ) {
         Loading(modifier = Modifier, visible = state.isLoading)
-        ConstraintLayout(
-            modifier = Modifier
-                .fillMaxWidth()
-                .border(width = 1.dp, color = Color.Black.copy(alpha = 0.05f))
-        ) {
-            val (dropDown, searchButton) = createRefs()
-            val dropDownData =
-                arrayOf("Semua", "Cappuccino", "Espresso", "Latte", "Mocha")
-            var expanded by remember { mutableStateOf(false) }
-            var selectedText by remember { mutableStateOf(dropDownData[0]) }
 
-            DropdownMenu(modifier = Modifier.constrainAs(dropDown) {
-                top.linkTo(parent.top)
-                start.linkTo(parent.start)
-                bottom.linkTo(parent.bottom)
-                width = Dimension.matchParent
-            },
-                data = dropDownData,
-                onItemSelected = {
-                    selectedText = it
-                    Toast.makeText(context, "Selected: $it", Toast.LENGTH_SHORT).show()
-                },
-                selectedText = selectedText,
-                expanded = expanded,
-                onExpandedChange = { expanded = it }
-            )
+        TextField(
+            value = "Cari",
+            onValueChanged = {},
+            modifier = Modifier,
+            title = "Cari",
+            textStyle = MaterialTheme.typography.titleMedium.copy(color = MaterialTheme.colorScheme.onBackground)
+        )
 
-            IconButton(
-                onClick = { /*TODO*/ },
-                modifier = Modifier
-                    .border(width = 1.dp, color = Color.Black.copy(alpha = 0.05f))
-                    .constrainAs(searchButton) {
-                        top.linkTo(parent.top)
-                        end.linkTo(parent.end)
-                        bottom.linkTo(parent.bottom)
-                        height = Dimension.fillToConstraints
-                        width = Dimension.wrapContent
-                    },
-                icon = Icons.Outlined.Search
-            )
-        }
         ConstraintLayout(
             modifier = Modifier
                 .fillMaxWidth()

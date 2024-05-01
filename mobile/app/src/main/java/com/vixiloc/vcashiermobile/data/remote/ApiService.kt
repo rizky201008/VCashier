c11d4659900b32183a66c62f2da1767f11082150
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-01 23:20:22 +0700

    mobile: add headers on all request

--- mobile/app/src/main/java/com/vixiloc/vcashiermobile/data/remote/ApiService.kt
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/data/remote/ApiService.kt
@@ -8,14 +8,17 @@ import com.vixiloc.vcashiermobile.data.remote.dto.ProductsResponseDto
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface ApiService {
 
     @POST(LOGIN)
+    @Headers("Content-Type: application/json","Accept: application/json")
     suspend fun login(@Body data: LoginRequestDto): LoginResponseDto
 
     @GET(PRODUCTS)
+    @Headers("Content-Type: application/json","Accept: application/json")
     suspend fun getProducts(
         @Header("Authorization") token: String,
     ): ProductsResponseDto

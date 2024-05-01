59b361dfd9943a3250dbb1721d3d8cd3f9769578
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-01 23:20:57 +0700

    mobile: modify DrawerMenuName

--- mobile/app/src/main/java/com/vixiloc/vcashiermobile/domain/model/DrawerMenu.kt
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/domain/model/DrawerMenu.kt
@@ -1,12 +1,14 @@
 package com.vixiloc.vcashiermobile.domain.model
 
 import androidx.compose.ui.graphics.vector.ImageVector
+import com.vixiloc.vcashiermobile.presentation.navigations.Screens
 
 data class DrawerMenu(
     val name: DrawerMenuName,
     val icon: ImageVector
 )
 
-enum class DrawerMenuName(val menuName: String) {
-    HOME("Home")
+enum class DrawerMenuName(val menuName: String, val route: String) {
+    HOME("Home", route = Screens.Home.route),
+    PRODUCTS("Transaksi", route = Screens.CreateTransaction.route),
 }

5e5739494cd4589657cb0e51ea13397b288e35ef
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-02 20:14:33 +0700

    mobile: create new method

--- mobile/app/src/main/java/com/vixiloc/vcashiermobile/domain/repository/CategoryRepository.kt
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/domain/repository/CategoryRepository.kt
@@ -1,8 +1,15 @@
 package com.vixiloc.vcashiermobile.domain.repository
 
 import com.vixiloc.vcashiermobile.data.remote.dto.CategoriesResponseDto
+import com.vixiloc.vcashiermobile.data.remote.dto.CreateCategoryRequestDto
+import com.vixiloc.vcashiermobile.data.remote.dto.CreateCategoryResponseDto
 
 interface CategoryRepository {
 
     suspend fun getCategories(token: String): CategoriesResponseDto
+
+    suspend fun createCategory(
+        token: String,
+        data: CreateCategoryRequestDto
+    ): CreateCategoryResponseDto
 }

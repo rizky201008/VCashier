03fd3fc71b151a7875d9c0703d4286f6b89e57d1
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-02 10:26:36 +0700

    mobile: modify CategoriesScreen

--- mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/screens/category/CategoriesScreen.kt
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/screens/category/CategoriesScreen.kt
@@ -1,25 +1,38 @@
 package com.vixiloc.vcashiermobile.presentation.screens.category
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
+import androidx.navigation.NavHostController
+import com.vixiloc.vcashiermobile.domain.model.CategoriesResponseItem
 import com.vixiloc.vcashiermobile.presentation.widgets.category.CategoryItem
+import com.vixiloc.vcashiermobile.presentation.widgets.commons.AlertType
 import com.vixiloc.vcashiermobile.presentation.widgets.commons.FilledButton
+import com.vixiloc.vcashiermobile.presentation.widgets.commons.Loading
+import com.vixiloc.vcashiermobile.presentation.widgets.commons.MessageAlert
 import com.vixiloc.vcashiermobile.presentation.widgets.commons.TextField
 import com.vixiloc.vcashiermobile.presentation.widgets.commons.VerticalSpacer
+import org.koin.androidx.compose.koinViewModel
 
-@Preview
 @Composable
-fun CategoriesScreen(modifier: Modifier = Modifier) {
-    ConstraintLayout {
-        val (searchInput, categories, addButton, addButtonSpacer) = createRefs()
+fun CategoriesScreen(
+    modifier: Modifier = Modifier,
+    navController: NavHostController,
+    viewModel: CategoryViewModel = koinViewModel()
+) {
+    val state = viewModel.state
+    val events = viewModel::onEvent
+
+    ConstraintLayout(modifier = modifier) {
+        val (searchInput, categories, addButton) = createRefs()
 
         TextField(
             value = "Cari",
@@ -33,39 +46,45 @@ fun CategoriesScreen(modifier: Modifier = Modifier) {
             textStyle = MaterialTheme.typography.titleMedium.copy(color = MaterialTheme.colorScheme.onBackground)
         )
 
-        FilledButton(
-            onClick = { /*TODO*/ },
-            text = "Tambah Kategori",
-            modifier = Modifier.constrainAs(addButton) {
-                top.linkTo(searchInput.bottom)
-                start.linkTo(searchInput.start, margin = 10.dp)
-                end.linkTo(searchInput.end, margin = 10.dp)
-                width = Dimension.fillToConstraints
-            })
-
-        VerticalSpacer(height = 10.dp, modifier = Modifier.constrainAs(addButtonSpacer) {
-            top.linkTo(addButton.bottom)
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-        })
-
         LazyColumn(modifier = Modifier
             .fillMaxWidth()
+            .height(500.dp)
             .constrainAs(categories) {
-                top.linkTo(addButtonSpacer.bottom)
+                top.linkTo(searchInput.bottom)
                 start.linkTo(parent.start)
                 end.linkTo(parent.end)
             }) {
-            items(10) {
+            items(state.categories) { category: CategoriesResponseItem ->
                 CategoryItem(
-                    headline = "Test Category",
+                    headline = category.name,
                     modifier = Modifier
                         .padding(10.dp),
-                    onDelete = {}
+                    onDelete = {},
+                    onUpdate = {}
                 )
             }
             item { VerticalSpacer(height = 300.dp, modifier = Modifier) }
         }
 
+        FilledButton(
+            onClick = { /*TODO*/ },
+            text = "Tambah Kategori",
+            modifier = Modifier.constrainAs(addButton) {
+                top.linkTo(parent.bottom)
+                start.linkTo(parent.start, margin = 10.dp)
+                end.linkTo(parent.end, margin = 10.dp)
+                width = Dimension.matchParent
+            })
+
+        Loading(modifier = Modifier, visible = state.isLoading)
+
+        MessageAlert(
+            type = AlertType.ERROR,
+            message = state.error,
+            title = "Error",
+            modifier = Modifier,
+            visible = state.error.isNotBlank(),
+            onDismiss = { events(CategoryEvent.DismissAlertMessage) }
+        )
     }
 }

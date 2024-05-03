f7bb887b515d871fb2e96b4c9bc0038385b262b7
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-03 10:17:59 +0700

    mobile: modify CreateCategory class

--- mobile/app/src/main/java/com/vixiloc/vcashiermobile/domain/use_case/CreateCategory.kt
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/domain/use_case/CreateCategory.kt
@@ -2,9 +2,9 @@ package com.vixiloc.vcashiermobile.domain.use_case
 
 import com.vixiloc.vcashiermobile.commons.Resource
 import com.vixiloc.vcashiermobile.data.remote.dto.toOnlyResponseMessage
-import com.vixiloc.vcashiermobile.domain.model.CreateCategoryRequest
+import com.vixiloc.vcashiermobile.domain.model.CreateUpdateCategoryRequest
 import com.vixiloc.vcashiermobile.domain.model.OnlyResponseMessage
-import com.vixiloc.vcashiermobile.domain.model.toCreateCategoryRequestDto
+import com.vixiloc.vcashiermobile.domain.model.toCreateUpdateCategoryRequestDto
 import com.vixiloc.vcashiermobile.domain.repository.CategoryRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
@@ -13,11 +13,11 @@ import okio.IOException
 import retrofit2.HttpException
 
 class CreateCategory(private val repository: CategoryRepository, private val getToken: GetToken) {
-    operator fun invoke(data:CreateCategoryRequest): Flow<Resource<OnlyResponseMessage>> = flow {
+    operator fun invoke(data:CreateUpdateCategoryRequest): Flow<Resource<OnlyResponseMessage>> = flow {
         val token: String = getToken().first()
         try {
             emit(Resource.Loading())
-            val response = repository.createCategory(token, data.toCreateCategoryRequestDto())
+            val response = repository.createCategory(token, data.toCreateUpdateCategoryRequestDto())
             emit(Resource.Success(response.toOnlyResponseMessage()))
         } catch (e: HttpException) {
             emit(Resource.Error(e.localizedMessage ?: "An unexpected error occurred"))

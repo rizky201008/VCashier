cf476ea70e8c9c05eb6db141d566ec6dba423ffe
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-04 22:48:17 +0700

    mobile: create CustomerState class

---
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/screens/customer/CustomerState.kt
@@ -0,0 +1,11 @@
+package com.vixiloc.vcashiermobile.presentation.screens.customer
+
+import com.vixiloc.vcashiermobile.domain.model.CustomerResponseItem
+
+data class CustomerState(
+    val isLoading: Boolean = false,
+    val customers: List<CustomerResponseItem> = emptyList(),
+    val error: String = "",
+    val customerName: String = "",
+    val success: String = ""
+)

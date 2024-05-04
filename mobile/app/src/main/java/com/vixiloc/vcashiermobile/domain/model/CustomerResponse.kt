8da8f84d100abb40beee9666ca8ef4fad95b6dfa
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-04 22:46:18 +0700

    mobile: create new model data class

---
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/domain/model/CustomerResponse.kt
@@ -0,0 +1,15 @@
+package com.vixiloc.vcashiermobile.domain.model
+
+import com.google.gson.annotations.SerializedName
+
+data class CustomerResponse(
+    val data: List<CustomerResponseItem> = emptyList()
+)
+
+data class CustomerResponseItem(
+    val updatedAt: String? = null,
+    val name: String? = null,
+    val createdAt: String? = null,
+    val phoneNumber: String? = null,
+    val id: Int? = null
+)

7cc918e0b0f291033a0f2e82d1295470f29d8dcc
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-05 01:14:32 +0700

    mobile: create CreateCustomerScreen

---
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/screens/customer/CreateCustomerScreen.kt
@@ -0,0 +1,49 @@
+package com.vixiloc.vcashiermobile.presentation.screens.customer
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.vixiloc.vcashiermobile.presentation.widgets.commons.AlertType
+import com.vixiloc.vcashiermobile.presentation.widgets.commons.Loading
+import com.vixiloc.vcashiermobile.presentation.widgets.commons.MessageAlert
+import org.koin.androidx.compose.koinViewModel
+
+
+@Composable
+fun CreateCustomerScreen(
+    modifier: Modifier = Modifier,
+    navController: NavController,
+    viewModel: CustomerViewModel = koinViewModel()
+) {
+    val state = viewModel.state
+    val events = viewModel::onEvent
+
+    Column(modifier = modifier) {
+        CustomerForm(viewModel = viewModel, type = FormType.CREATE)
+
+        MessageAlert(
+            type = AlertType.SUCCESS,
+            message = state.success,
+            title = "Sukses",
+            modifier = Modifier,
+            visible = state.success.isNotEmpty(),
+            onDismiss = {
+                events(CustomerEvent.DismissAlertMessage)
+                navController.navigateUp()
+            }
+        )
+        MessageAlert(
+            type = AlertType.ERROR,
+            message = state.error,
+            title = "Error",
+            modifier = Modifier,
+            visible = state.error.isNotEmpty(),
+            onDismiss = {
+                events(CustomerEvent.DismissAlertMessage)
+            }
+        )
+
+        Loading(modifier = Modifier, visible = state.isLoading)
+    }
+}

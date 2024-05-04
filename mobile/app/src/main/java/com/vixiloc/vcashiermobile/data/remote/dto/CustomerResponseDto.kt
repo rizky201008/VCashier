952744ff1b80985fa0411470db62f9047e89d5cd
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-04 22:45:06 +0700

    mobile: create new dto classes

---
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/data/remote/dto/CustomerResponseDto.kt
@@ -0,0 +1,41 @@
+package com.vixiloc.vcashiermobile.data.remote.dto
+
+import com.google.gson.annotations.SerializedName
+import com.vixiloc.vcashiermobile.domain.model.CustomerResponse
+import com.vixiloc.vcashiermobile.domain.model.CustomerResponseItem
+
+data class CustomerResponseDto(
+
+    @field:SerializedName("data")
+    val data: List<CustomerResponseDtoItem> = emptyList()
+)
+
+data class CustomerResponseDtoItem(
+
+    @field:SerializedName("updated_at")
+    val updatedAt: String? = null,
+
+    @field:SerializedName("name")
+    val name: String? = null,
+
+    @field:SerializedName("created_at")
+    val createdAt: String? = null,
+
+    @field:SerializedName("phone_number")
+    val phoneNumber: String? = null,
+
+    @field:SerializedName("id")
+    val id: Int? = null
+)
+
+fun CustomerResponseDtoItem.toCustomerResponseItem() = CustomerResponseItem(
+    updatedAt = updatedAt,
+    name = name,
+    createdAt = createdAt,
+    phoneNumber = phoneNumber,
+    id = id
+)
+
+fun CustomerResponseDto.toCustomerResponse() = CustomerResponse(
+    data = data.map { it.toCustomerResponseItem() }
+)

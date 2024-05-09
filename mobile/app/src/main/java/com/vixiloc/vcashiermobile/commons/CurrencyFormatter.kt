6ac392c0d4c23ccf0b5afddd2b6fb21890370a0a
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-09 14:04:13 +0700

    mobile: create CurrencyFormatter class

---
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/commons/CurrencyFormatter.kt
@@ -0,0 +1,14 @@
+package com.vixiloc.vcashiermobile.commons
+
+import java.text.NumberFormat
+import java.util.Currency
+
+class CurrencyFormatter {
+    fun formatCurrency(amount: Int): String {
+        val format: NumberFormat = NumberFormat.getCurrencyInstance()
+        format.setMaximumFractionDigits(0)
+        format.setCurrency(Currency.getInstance("IDR"))
+
+        return format.format(amount)
+    }
+}

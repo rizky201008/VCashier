5995559d9928087f6d43b6784f9cc61f770290ae
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-09 14:06:27 +0700

    mobile: modify ProductsResponse

--- mobile/app/src/main/java/com/vixiloc/vcashiermobile/domain/model/ProductsResponse.kt
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/domain/model/ProductsResponse.kt
@@ -1,32 +1,27 @@
 package com.vixiloc.vcashiermobile.domain.model
 
-import com.google.gson.annotations.SerializedName
-
 data class ProductsResponse(
     val data: List<ProductResponseItems?>? = null
 )
 
 data class Category(
-    val name: String? = null,
-    val id: Int? = null
+    val name: String,
+    val id: Int
 )
 
 data class Product(
-    val categoryId: Int? = null,
-    val imagePath: String? = null,
     val imageUrl: String? = null,
-    val name: String? = null,
-    val description: String? = null,
-    val id: Int? = null,
-    val category: Category? = null
+    val name: String,
+    val description: String,
+    val id: Int,
+    val category: Category
 )
 
 data class ProductResponseItems(
     val priceGrocery: Int? = null,
-    val unit: String? = null,
-    val product: Product? = null,
-    val price: Int? = null,
-    val productId: Int? = null,
-    val id: Int? = null,
-    val stock: Int? = null
+    val unit: String,
+    val product: Product,
+    val price: Int,
+    val id: Int,
+    val stock: Int
 )

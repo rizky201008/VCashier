6357a22432b1c578243367e759e4d64c4e674011
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-16 16:52:06 +0700

    mobile: create RequestBodyBuilder object class

---
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/commons/RequestBodyBuilder.kt
@@ -0,0 +1,13 @@
+package com.vixiloc.vcashiermobile.commons
+
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+
+object RequestBodyBuilder {
+    fun createString(
+        data: String
+    ): RequestBody {
+        return data.toRequestBody("text/plain".toMediaType())
+    }
+}

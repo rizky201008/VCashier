1bff72322befb6843790cdc773b772164bccba50
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-17 09:48:46 +0700

    mobile: create new event

--- mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/screens/products/ProductEvent.kt
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/screens/products/ProductEvent.kt
@@ -1,13 +1,24 @@
 package com.vixiloc.vcashiermobile.presentation.screens.products
 
-import com.vixiloc.vcashiermobile.domain.model.ProductResponseItems
+import android.net.Uri
 
 
 sealed class ProductEvent {
     data object DismissAlertMessage : ProductEvent()
-//    data class InputProductName(val name: String) : ProductEvent()
-//    data class InputProductNumber(val number: String) : ProductEvent()
-//    data object SubmitCreateProduct : ProductEvent()
+    data class InputProductName(val name: String) : ProductEvent()
+    data class InputProductDescription(val description: String) : ProductEvent()
+    data class InputProductCategory(val category: Int) : ProductEvent()
+    data class InputVariationUnit(val unit: String) : ProductEvent()
+    data class InputVariationPrice(val price: String) : ProductEvent()
+    data class InputVariationPriceGrocery(val price: String) : ProductEvent()
+    data class InputVariationStock(val stock: String) : ProductEvent()
+    data class InputProductImage(val image: Uri?) : ProductEvent()
+    data class InputProductCategoryName(val name: String) : ProductEvent()
+
+    data object ToggleVariationDialog : ProductEvent()
+    data object SubmitAddVariation : ProductEvent()
+
+    data object SubmitCreateProduct : ProductEvent()
 //    data object SubmitUpdateProduct : ProductEvent()
 //    data class PreFillFormData(val id: Int?, val name: String, val number: String?) : ProductEvent()
 //    data class DeleteProduct(val data: ProductResponseItems) : ProductEvent()

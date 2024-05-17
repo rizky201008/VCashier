2f820ae7bc165994139ff9948781d2659c5088b3
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-17 11:11:42 +0700

    backend: modify imageUrl accessor

--- backend/app/Models/Product.php
+++ backend/app/Models/Product.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Storage;
 
 class Product extends Model
 {
@@ -25,14 +27,21 @@ class Product extends Model
         'updated_at'
     ];
 
-    public function variations() : HasMany
+    public function variations(): HasMany
     {
         return $this->hasMany(ProductVariation::class);
     }
 
-    public function category() : BelongsTo
+    public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    protected function imageUrl(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => ($value) ? Storage::url("product/$value") : null,
+        );
     }
 
 }

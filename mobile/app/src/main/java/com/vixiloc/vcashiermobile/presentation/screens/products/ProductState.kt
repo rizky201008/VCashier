7905510172825a5043e60f093e88e306f0b1b3e0
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-17 09:49:19 +0700

    mobile: create new state

--- mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/screens/products/ProductState.kt
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/screens/products/ProductState.kt
@@ -1,10 +1,25 @@
 package com.vixiloc.vcashiermobile.presentation.screens.products
 
+import android.net.Uri
 import com.vixiloc.vcashiermobile.domain.model.ProductResponseItems
+import com.vixiloc.vcashiermobile.domain.model.Variation
 
 data class ProductState(
     val products: List<ProductResponseItems> = emptyList(),
     val isLoading: Boolean = false,
     val error: String = "",
     val success: String = "",
+    val productName: String = "",
+    val productDescription: String = "",
+    val productCategory: Int = 0,
+    val variations: List<Variation> = emptyList(),
+    val image: Uri? = null,
+    val categoryName: String = "",
+    val variationProductId: Int? = null,
+    val variationPrice: String = "",
+    val variationPriceGrocery: String = "",
+    val variationStock: String = "",
+    val variationUnit: String = "",
+    val showVariationDialog: Boolean = false,
+    val categories: List<Map<String, Int>> = emptyList()
 )

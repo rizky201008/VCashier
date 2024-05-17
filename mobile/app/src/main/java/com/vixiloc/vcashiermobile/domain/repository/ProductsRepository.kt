8639aac425a7fbc093c6b9a997530fb44efa586a
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-17 09:44:51 +0700

    mobile: create new method

--- mobile/app/src/main/java/com/vixiloc/vcashiermobile/domain/repository/ProductsRepository.kt
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/domain/repository/ProductsRepository.kt
@@ -1,8 +1,30 @@
 package com.vixiloc.vcashiermobile.domain.repository
 
+import com.vixiloc.vcashiermobile.data.remote.dto.CreateProductResponseDto
+import com.vixiloc.vcashiermobile.data.remote.dto.CreateUpdateImageResponseDto
+import com.vixiloc.vcashiermobile.data.remote.dto.CreateUpdateProductRequestDto
 import com.vixiloc.vcashiermobile.data.remote.dto.ProductsResponseDto
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 
 interface ProductsRepository {
 
     suspend fun getProducts(token: String): ProductsResponseDto
+
+    suspend fun createProduct(
+        token: String,
+        data: CreateUpdateProductRequestDto
+    ): CreateProductResponseDto
+
+    suspend fun addImage(
+        token: String,
+        productId: RequestBody,
+        image: MultipartBody.Part
+    ): CreateUpdateImageResponseDto
+
+    suspend fun updateImage(
+        token: String,
+        data: RequestBody
+    ): CreateUpdateImageResponseDto
+
 }

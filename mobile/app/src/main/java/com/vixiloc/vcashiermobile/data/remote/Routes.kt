a9d8cb70d22ee8d8b5f2423226ae7ad84eaeb1fb
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-18 13:22:18 +0700

    mobile: modify Routes.kt

--- mobile/app/src/main/java/com/vixiloc/vcashiermobile/data/remote/Routes.kt
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/data/remote/Routes.kt
@@ -2,7 +2,7 @@ package com.vixiloc.vcashiermobile.data.remote
 
 object Routes {
 
-    const val BASE_URL = "http://10.0.2.2:8001/api/"
+    const val BASE_URL = "http://10.0.2.2:8000/api/"
     const val LOGIN = "auth/login"
     const val PRODUCTS = "products"
     const val CATEGORIES = "categories"

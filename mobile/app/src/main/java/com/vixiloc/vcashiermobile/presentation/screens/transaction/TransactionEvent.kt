b66f60fa657ce6c8672fe025569cd83ac26462ea
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-20 17:37:57 +0700

    mobile: create new event

--- mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/screens/transaction/TransactionEvent.kt
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/screens/transaction/TransactionEvent.kt
@@ -1,10 +1,17 @@
 package com.vixiloc.vcashiermobile.presentation.screens.transaction
 
+import com.vixiloc.vcashiermobile.domain.model.CustomerResponseItem
 import com.vixiloc.vcashiermobile.domain.model.ProductResponseItems
 
 sealed class TransactionEvent {
-    data class ToggleError(val error: String) : TransactionEvent()
     data object Refresh : TransactionEvent()
-
+    data object ClearCart : TransactionEvent()
     data class SelectProduct(val product: ProductResponseItems) : TransactionEvent()
+    data class IncreaseQty(val product: ProductResponseItems) : TransactionEvent()
+    data class DecreaseQty(val product: ProductResponseItems) : TransactionEvent()
+    data class UpdateCustomer(val customer: CustomerResponseItem?) : TransactionEvent()
+    data object DismissAlertMessage : TransactionEvent()
+    data object SubmitTransaction : TransactionEvent()
+    data class InserSelectedProducts(val products: List<Map<ProductResponseItems, Int>>) :
+        TransactionEvent()
 }

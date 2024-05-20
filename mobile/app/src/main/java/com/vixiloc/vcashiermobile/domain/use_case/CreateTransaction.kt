c4f10ede5927bd86372969af854ea3936af0d3ff
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-20 17:36:35 +0700

    mobile: create new use case class

---
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/domain/use_case/CreateTransaction.kt
@@ -0,0 +1,40 @@
+package com.vixiloc.vcashiermobile.domain.use_case
+
+import com.vixiloc.vcashiermobile.commons.HttpHandler
+import com.vixiloc.vcashiermobile.commons.Resource
+import com.vixiloc.vcashiermobile.data.remote.dto.toDomain
+import com.vixiloc.vcashiermobile.domain.model.CreateTransactionRequest
+import com.vixiloc.vcashiermobile.domain.model.OnlyResponseMessage
+import com.vixiloc.vcashiermobile.domain.model.toDto
+import com.vixiloc.vcashiermobile.domain.repository.TransactionRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import okio.IOException
+import retrofit2.HttpException
+
+class CreateTransaction(
+    private val repository: TransactionRepository,
+    private val getToken: GetToken,
+    private val httpHandler: HttpHandler
+) {
+    operator fun invoke(data: CreateTransactionRequest): Flow<Resource<OnlyResponseMessage>> =
+        flow {
+            val token = getToken().first()
+            try {
+                emit(Resource.Loading())
+                val response = repository.createTransaction(token = token, data = data.toDto())
+                emit(Resource.Success(response.toDomain()))
+            } catch (e: HttpException) {
+                val errorMessage = httpHandler.handleHttpException(e)
+                emit(Resource.Error(errorMessage))
+            } catch (e: IOException) {
+                emit(
+                    Resource.Error(
+                        e.localizedMessage
+                            ?: "Couldn't reach server. Check your internet connection."
+                    )
+                )
+            }
+        }
+}

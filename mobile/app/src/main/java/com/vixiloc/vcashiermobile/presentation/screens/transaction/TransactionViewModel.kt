00ad8ac4ec35930c9e80e503fe6b9ec86bfed7ae
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-20 17:39:26 +0700

    mobile: modify viewmodel

--- mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/screens/transaction/TransactionViewModel.kt
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/screens/transaction/TransactionViewModel.kt
@@ -1,75 +1,189 @@
 package com.vixiloc.vcashiermobile.presentation.screens.transaction
 
-import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.vixiloc.vcashiermobile.commons.Resource
-import com.vixiloc.vcashiermobile.commons.Strings
-import com.vixiloc.vcashiermobile.commons.Strings.TAG
+import com.vixiloc.vcashiermobile.domain.model.CreateTransactionRequest
+import com.vixiloc.vcashiermobile.domain.model.CustomerResponseItem
+import com.vixiloc.vcashiermobile.domain.model.Item
+import com.vixiloc.vcashiermobile.domain.use_case.CreateTransaction
+import com.vixiloc.vcashiermobile.domain.use_case.GetCustomers
 import com.vixiloc.vcashiermobile.domain.use_case.GetProducts
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
-import kotlin.math.log
 
-class TransactionViewModel(private val getProductsUseCase: GetProducts) : ViewModel() {
+class TransactionViewModel(
+    private val getProductsUseCase: GetProducts,
+    private val createTransactionUseCase: CreateTransaction,
+    private val getCustomerUseCase: GetCustomers
+) : ViewModel() {
 
     var state by mutableStateOf(TransactionState())
-    var pageShowedTime = 0
-
-    init {
-        getProducts()
-    }
 
     fun onEvent(event: TransactionEvent) {
         when (event) {
-            is TransactionEvent.ToggleError -> {
-                toggleError(event.error)
-            }
 
             is TransactionEvent.Refresh -> {
-                pageShowedTime++
-                if (pageShowedTime > 1) {
-                    getProducts()
-                }
+                getProducts()
+            }
+
+            is TransactionEvent.ClearCart -> {
+                resetCart()
             }
 
             is TransactionEvent.SelectProduct -> {
-                if (!state.selectedProduct.contains(event.product)) {
-                    state = state.copy(selectedProduct = state.selectedProduct.plus(event.product))
+                if (!state.selectedProduct.any { it.containsKey(event.product) }) {
+                    state = state.copy(
+                        selectedProduct = state.selectedProduct.plus(mapOf(event.product to 1)),
+                        totalPrice = state.totalPrice + event.product.price
+                    )
                 }
             }
+
+            is TransactionEvent.IncreaseQty -> {
+                val product = state.selectedProduct.first { it.containsKey(event.product) }
+                val quantity = product[event.product] ?: 0
+                state = state.copy(
+                    selectedProduct = state.selectedProduct.map {
+                        if (it.containsKey(event.product)) {
+                            mapOf(event.product to quantity + 1)
+                        } else {
+                            it
+                        }
+                    },
+                    totalPrice = state.totalPrice + event.product.price
+                )
+            }
+
+            is TransactionEvent.DecreaseQty -> {
+                val product = state.selectedProduct.first { it.containsKey(event.product) }
+                val quantity = product[event.product] ?: 0
+                if (quantity > 1) {
+                    state = state.copy(
+                        selectedProduct = state.selectedProduct.map {
+                            if (it.containsKey(event.product)) {
+                                mapOf(event.product to quantity - 1)
+                            } else {
+                                it
+                            }
+                        },
+                        totalPrice = state.totalPrice - event.product.price
+                    )
+                } else {
+                    state = state.copy(
+                        selectedProduct = state.selectedProduct.filter { !it.containsKey(event.product) },
+                        totalPrice = state.totalPrice - event.product.price
+                    )
+                }
+            }
+
+            is TransactionEvent.UpdateCustomer -> {
+                state = state.copy(
+                    customer = event.customer ?: CustomerResponseItem(
+                        id = 1,
+                        name = "Tamu",
+                        phoneNumber = "-"
+                    )
+                )
+            }
+
+            is TransactionEvent.DismissAlertMessage -> {
+                state = state.copy(error = "", success = "")
+            }
+
+            is TransactionEvent.SubmitTransaction -> {
+                createTransaction()
+            }
+
+            is TransactionEvent.InserSelectedProducts -> {
+                state = state.copy(selectedProduct = event.products)
+            }
         }
+    }
+
+    private fun resetCart() {
+        state = state.copy(selectedProduct = emptyList(), totalPrice = 0)
+    }
+
+    fun getCustomers() {
+        getCustomerUseCase().onEach { resource ->
+            when (resource) {
+                is Resource.Loading -> {
+                    state = state.copy(isLoading = true)
+                }
+
+                is Resource.Success -> {
+                    state = state.copy(customers = resource.data ?: emptyList(), isLoading = false)
+                }
+
+                is Resource.Error -> {
+                    state = state.copy(
+                        isLoading = false,
+                        error = resource.message ?: "An unexpected error occurred"
+                    )
+                }
+            }
+        }.launchIn(viewModelScope)
     }
 
     private fun getProducts() {
         getProductsUseCase().onEach { resource ->
             when (resource) {
                 is Resource.Loading -> {
-                    toggleLoading()
+                    state = state.copy(isLoading = true)
                 }
 
                 is Resource.Success -> {
-                    toggleLoading()
-                    Log.d(TAG, "getProducts: ${resource.data}")
-                    state = state.copy(products = resource.data ?: emptyList())
+                    state = state.copy(
+                        isLoading = false,
+                        products = resource.data ?: emptyList()
+                    )
                 }
 
                 is Resource.Error -> {
-                    toggleLoading()
-                    toggleError(resource.message ?: "An unexpected error occurred")
+                    state = state.copy(
+                        isLoading = false,
+                        error = resource.message ?: "An unexpected error occurred"
+                    )
                 }
             }
         }.launchIn(viewModelScope)
     }
 
-    private fun toggleLoading() {
-        state = state.copy(isLoading = !state.isLoading)
-    }
+    private fun createTransaction() {
+        val items = state.selectedProduct.flatMap { map ->
+            map.map { (product, quantity) ->
+                Item(id = product.id, quantity = quantity)
+            }
+        }
+        val data = CreateTransactionRequest(
+            customerId = state.customer.id,
+            items = items
+        )
 
-    private fun toggleError(error: String) {
-        state = state.copy(error = error)
+        createTransactionUseCase(data).onEach { resource ->
+            when (resource) {
+                is Resource.Loading -> {
+                    state = state.copy(isLoading = true)
+                }
+
+                is Resource.Success -> {
+                    state = state.copy(
+                        success = resource.data?.message ?: "Transaction created successfully",
+                        isLoading = false
+                    )
+                }
+
+                is Resource.Error -> {
+                    state = state.copy(
+                        isLoading = false,
+                        error = resource.message ?: "An unexpected error occurred"
+                    )
+                }
+            }
+        }.launchIn(viewModelScope)
     }
 }

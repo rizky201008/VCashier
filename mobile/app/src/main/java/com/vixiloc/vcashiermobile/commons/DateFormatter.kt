141dd861426274a66beb2d79aa0ae6d6b041919a
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-05-23 18:20:24 +0700

    mobile: create new object classes

---
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/commons/DateFormatter.kt
@@ -0,0 +1,25 @@
+package com.vixiloc.vcashiermobile.commons
+
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
+object DateFormatter {
+
+    fun format(date: String): String {
+
+        // Parser untuk format timestamp
+        val formatter = DateTimeFormatter.ISO_ZONED_DATE_TIME
+
+        // Parsing timestamp ke ZonedDateTime
+        val utcDateTime = ZonedDateTime.parse(date, formatter)
+
+        // Konversi ke zona waktu lokal
+        val localDateTime = utcDateTime.withZoneSameInstant(ZoneId.systemDefault())
+
+        // Format hasil ke format yang diinginkan
+        val outputFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")
+        val formattedDateTime = localDateTime.format(outputFormatter)
+        return formattedDateTime
+    }
+}

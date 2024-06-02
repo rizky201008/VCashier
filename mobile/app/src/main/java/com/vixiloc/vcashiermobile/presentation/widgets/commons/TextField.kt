fddf5ad8831c722e20a4b8bc6ee4ba6c7e906b9b
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-06-02 16:11:14 +0700

    mobile: optimize imports and modify components

--- mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/widgets/commons/TextField.kt
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/widgets/commons/TextField.kt
@@ -1,5 +1,7 @@
 package com.vixiloc.vcashiermobile.presentation.widgets.commons
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,17 +10,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -26,9 +21,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.vixiloc.vcashiermobile.presentation.widgets.home.DropdownMenu
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TextField(
     value: String,
@@ -39,11 +32,13 @@ fun TextField(
     keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
     textStyle: TextStyle = MaterialTheme.typography.bodySmall,
     singleLine: Boolean = true,
+    enabled: Boolean = true,
+    isError: Boolean = false,
+    errorMessage: String = ""
 ) {
     Column(
         modifier = modifier
             .fillMaxWidth()
-            .padding(10.dp)
     ) {
         Text(text = title, style = textStyle)
         VerticalSpacer(height = 9.dp)
@@ -64,9 +59,18 @@ fun TextField(
                     .fillMaxWidth()
                     .padding(10.dp),
                 singleLine = singleLine,
-                textStyle = MaterialTheme.typography.bodySmall,
+                textStyle = textStyle,
                 visualTransformation = visualTransformation,
                 keyboardOptions = keyboardOptions,
+                enabled = enabled
+            )
+        }
+        val errorVisibleState = remember { MutableTransitionState(isError) }
+        AnimatedVisibility(visibleState = errorVisibleState) {
+            Text(
+                text = errorMessage,
+                style = MaterialTheme.typography.bodySmall.copy(color = Color.Red),
+                modifier = Modifier.padding(start = 10.dp)
             )
         }
     }

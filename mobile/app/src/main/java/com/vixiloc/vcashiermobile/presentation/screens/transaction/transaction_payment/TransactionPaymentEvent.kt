1679981080f7a692c0f3615788a5d5a006ac7901
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-06-09 12:56:40 +0700

    mobile: create event, state, and viewModel classes

---
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/screens/transaction/transaction_payment/TransactionPaymentEvent.kt
@@ -0,0 +1,11 @@
+package com.vixiloc.vcashiermobile.presentation.screens.transaction.transaction_payment
+
+import com.vixiloc.vcashiermobile.domain.model.PaymentMethodData
+
+sealed class TransactionPaymentEvent {
+    data object DismissAlertMessage : TransactionPaymentEvent()
+    data class InsertTransactionTotal(val total: Int) : TransactionPaymentEvent()
+    data class UpdatePaymentAmount(val amount: String) : TransactionPaymentEvent()
+    data class SelectPaymentMethod(val method: PaymentMethodData) : TransactionPaymentEvent()
+    data object SubmitTransactionPayment : TransactionPaymentEvent()
+}

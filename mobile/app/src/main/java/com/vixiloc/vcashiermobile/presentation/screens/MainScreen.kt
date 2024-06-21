f447715729bcf237d5e235158044587f832d6e24
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-06-21 16:24:36 +0700

    mobile: modify MainScreen TopBar

--- mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/screens/MainScreen.kt
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/screens/MainScreen.kt
@@ -5,11 +5,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.AllInbox
-import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material.icons.outlined.Group
 import androidx.compose.material.icons.outlined.Home
-import androidx.compose.material.icons.outlined.Menu
 import androidx.compose.material.icons.outlined.ShoppingCart
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -20,22 +19,27 @@ import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.vixiloc.vcashiermobile.R
 import com.vixiloc.vcashiermobile.domain.model.DrawerMenu
 import com.vixiloc.vcashiermobile.domain.model.DrawerMenuName
 import com.vixiloc.vcashiermobile.domain.model.DrawerMenuRoute
+import com.vixiloc.vcashiermobile.presentation.components.commons.PainterIconButton
 import com.vixiloc.vcashiermobile.presentation.navigations.MainNavHost
-import com.vixiloc.vcashiermobile.presentation.components.commons.IconButton
 import kotlinx.coroutines.launch
-
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -70,13 +74,28 @@ fun MainScreen(navHostController: NavHostController) {
         )
     )
 
+    // MutableState to hold the current title
+    val currentTitle = remember { mutableStateOf("VCashier") }
+
+    // Get the current back stack entry and the current route
+    val navBackStackEntry by navHostController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    // Find the matching DrawerMenu item based on the current route
+    currentRoute?.let {
+        val matchedItem = items.find { it.route == it }
+        matchedItem?.let {
+            currentTitle.value = it.name.menuName
+        }
+    }
+
     ModalNavigationDrawer(
         drawerState = drawerState,
         drawerContent = {
             ModalDrawerSheet {
-                IconButton(
+                PainterIconButton(
                     onClick = { scope.launch { drawerState.close() } },
-                    icon = Icons.Outlined.Close,
+                    icon = painterResource(id = R.drawable.hamburger_icon),
                     modifier = Modifier
                         .padding(10.dp)
                         .align(Alignment.End)
@@ -89,6 +108,7 @@ fun MainScreen(navHostController: NavHostController) {
                         selected = false,
                         onClick = {
                             scope.launch { drawerState.close() }
+                            currentTitle.value = item.name.menuName
                             navHostController.navigate(item.route) {
                                 popUpTo(navHostController.graph.findStartDestination().id) {
                                     saveState = true
@@ -105,16 +125,20 @@ fun MainScreen(navHostController: NavHostController) {
         content = {
             Scaffold(
                 topBar = {
-                    TopAppBar(title = {
-                        Text(
-                            text = "VCashier",
-                            style = MaterialTheme.typography.titleMedium.copy(color = MaterialTheme.colorScheme.primary)
-                        )
-                    }, navigationIcon = {
-                        IconButton(onClick = {
-                            scope.launch { drawerState.open() }
-                        }, icon = Icons.Outlined.Menu)
-                    })
+                    CenterAlignedTopAppBar(
+                        title = {
+                            Text(
+                                text = currentTitle.value,
+                                style = MaterialTheme.typography.titleMedium.copy(color = MaterialTheme.colorScheme.primary, fontWeight = FontWeight(600))
+                            )
+                        }, navigationIcon = {
+                            PainterIconButton(
+                                onClick = {
+                                    scope.launch { drawerState.open() }
+                                },
+                                icon = painterResource(id = R.drawable.hamburger_icon)
+                            )
+                        })
                 }
             ) { paddingValues ->
                 MainNavHost(

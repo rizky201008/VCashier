330825919d8cc57938937ba4a7e77ac6d1bedac4
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-07-11 17:46:45 +0700

    backend : create model and migration

---
+++ backend/database/migrations/2024_07_11_104039_create_product_logs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\ProductVariation;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('information');
+            $table->enum('type', ['increase', 'decrease']);
+            $table->integer('amount');
+            $table->foreignIdFor(ProductVariation::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_logs');
+    }
+};

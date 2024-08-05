0ad00b60a7d84d1198e83fedd116762e0e896e19
Rizky Agung Prayogi <rizkyden06@gmail.com>
2024-08-05 17:57:41 +0700

    mobile: modify typo

--- mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/screens/products/update_product/components/Modals.kt
+++ mobile/app/src/main/java/com/vixiloc/vcashiermobile/presentation/screens/products/update_product/components/Modals.kt
@@ -1,9 +1,7 @@
 package com.vixiloc.vcashiermobile.presentation.screens.products.update_product.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -21,13 +19,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
-import androidx.compose.ui.zIndex
 import androidx.core.text.isDigitsOnly
 import com.vixiloc.vcashiermobile.presentation.components.FilledButton
 import com.vixiloc.vcashiermobile.presentation.components.IconButton
@@ -65,7 +61,7 @@ fun EditVariationDialog(modifier: Modifier = Modifier, viewModel: UpdateProductV
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     Text(
-                        text = "Tambah Variasi",
+                        text = "Ubah Variasi",
                         style = MaterialTheme.typography.titleMedium.copy(
                             color = MaterialTheme.colorScheme.primary,
                             fontWeight = FontWeight(600)
